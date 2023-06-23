ad5f2e6caabdf0dc7c0e86b3e40766e80924ea1e
Zak Bay <zacharybay@live.com>
2023-06-23 13:17:12 -0700

    nodemon congiged

---
+++ prisma/seed.ts
@@ -0,0 +1,8 @@
+
+
+const seed = async () => {
+
+   
+
+
+}
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express'
-import "express-async-errors";
+// import "express-async-errors";
 import { userController } from './routers/user.router';
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 app.use(express.json());
+
+export const prisma = new PrismaClient();
+
+
 
 app.use(userController);
 
